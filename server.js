aa2e982e23e89b72877260be8cbf5f65664f49a9
Alessandro Fulgini <fuljo97@gmail.com>
2016-02-07 22:00:55 +0100

    Add user model and POST /users request

--- server.js
+++ server.js
@@ -112,7 +112,18 @@ app.put('/todos/:id', function (req, res) {
   }, function (e) {
     res.status(500).json(e);
   });
-})
+});
+
+// POST /users
+app.post('/users', function (req, res) {
+  var body = _.pick(req.body, 'email', 'password');
+
+  db.user.create(body).then(function (user) {
+    return res.json(user.toJSON());
+  }, function (e) {
+    res.status(400).json(e);
+  });
+});
 
 db.sequelize.sync(/*{force: true}*/).then(function () {
   app.listen(PORT, function () {

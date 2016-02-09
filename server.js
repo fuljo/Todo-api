ffec8038a828475dd454dec71b2b38b175f20acb
Alessandro Fulgini <fuljo97@gmail.com>
2016-02-09 19:08:40 +0100

    Hash and salt password

--- server.js
+++ server.js
@@ -119,13 +119,13 @@ app.post('/users', function (req, res) {
   var body = _.pick(req.body, 'email', 'password');
 
   db.user.create(body).then(function (user) {
-    return res.json(user.toJSON());
+    return res.json(user.toPublicJSON());
   }, function (e) {
     res.status(400).json(e);
   });
 });
 
-db.sequelize.sync(/*{force: true}*/).then(function () {
+db.sequelize.sync( {force: true} ).then(function () {
   app.listen(PORT, function () {
     console.log('Express listening on port '+ PORT);
   });
